44580b4a543151208a38aa7fa5e044cd612145b6
s-flores <00sfa1138@gmail.com>
2020-07-31 13:02:38 -0700

    initial commit

---
+++ includes/config.php
@@ -0,0 +1,11 @@
+<?php
+    ob_start();
+
+    $timeZone = date_default_timezone_set("America/Los_Angeles");
+
+    $con = mysqli_connect("localhost", "root", "", "project");
+
+    if(mysqli_connect_errno()){
+        echo "failed to connect: ".mysqli_connect_error();
+    }
+
