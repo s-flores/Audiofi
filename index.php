8525a7d805aaba53a2183d64979701eeda2c2f68
s-flores <00sfa1138@gmail.com>
2021-09-16 15:34:53 -0700

    Navigation menu complete

--- index.php
+++ index.php
@@ -26,16 +26,44 @@ else {
         <div id="topContainer">
 
             <div id="navBarContainer">
+
                 <nav class="navBar">
+
                     <a href="index.php" class="logo">
                         <img src="assets/images/icons/logo.png">
                     </a>
+
+                   <div class="group">
+
+                        <div class="navItem">
+                            <a href="search.php" class="navItemLink">Search
+                                <img src="assets/images/icons/search.png" class="icon" alt="search">
+                            </a>
+                        </div>
+
+                   </div> 
+
+                    <div class="group">
+                          <div class="navItem">
+                              <a href="browse.php" class="navItemLink">Browse</a>
+                        </div>
+
+                        <div class="navItem">
+                            <a href="yourMusic.php" class="navItemLink">Your Music</a>
+                        </div>
+
+                        <div class="navItem">
+                            <a href="profile.php" class="navItemLink">Profile</a> 
+                        </div>        
+                    </div> 
+
                 </nav>
+
             </div>
 
         </div>
 
-         <div id="nowPlayingBarContainer">
+        <div id="nowPlayingBarContainer">
 
             <div id="nowPlayingBar">
 
@@ -56,8 +84,6 @@ else {
                             </span>
 
                         </div>
-
-
 
                     </div>
                 </div>
