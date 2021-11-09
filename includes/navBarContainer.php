cc2f7399506e07594d509a67c3430c1b397f3941
s-flores <34001096+s-flores@users.noreply.github.com>
2021-11-08 21:59:53 -0800

    settings page completed

--- includes/navBarContainer.php
+++ includes/navBarContainer.php
@@ -28,7 +28,7 @@
             </div>
 
             <div class="navItem">
-            <span  role="link" tabindex="0" onclick="openPage('profile.php')" class="navItemLink">Profile</span> 
+            <span  role="link" tabindex="0" onclick="openPage('settings.php')" class="navItemLink"> <?php echo $userLoggedIn->getUsername(); ?> </span> 
             </div>        
         </div> 
 
